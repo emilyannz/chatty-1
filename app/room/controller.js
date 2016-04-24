c1b73dd7a7dccf7f8b58d40dfbd158c72cf8d68e
Nick Trull <nick.trull@gmail.com>
2016-04-24 17:10:37 -0500

    restyle. added concurrency

--- app/room/controller.js
+++ app/room/controller.js
@@ -1,9 +1,22 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
+  trigger() {
+    this.get(`updateMessages`).perform();
+  },
+
+  updateMessages: task(function * () {
+    while (true) {
+      yield timeout(2000);
+      console.log(`test`);
+      this.store.findAll(`message`);
+    }
+  }),
 
   sendMessage(attrs) {
     const newMessage = this.store.createRecord(`message`, { content: attrs });
+    this.trigger();
 
     newMessage.save()
     .then(() => {
